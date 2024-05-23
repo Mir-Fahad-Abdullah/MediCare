d66155160c3f515617b45c1f9159645ea979b0ad
Mir Fahad Abdullah <154448101+Mir-Fahad-Abdullah@users.noreply.github.com>
2024-05-23 15:42:45 +0600

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const db = require('./config/db');
-const PORT = process.env.PORT || 3000;
-
-db.connect();
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
